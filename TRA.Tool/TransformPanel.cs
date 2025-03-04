0a72c9d21bde4f4120e0f743704ef9bb6affbfac
EMS90 <108662825+EMS90@users.noreply.github.com>
2025-03-04 10:37:11 +0100

    Added additional Transforms

--- TRA.Tool/TransformPanel.cs
+++ TRA.Tool/TransformPanel.cs
@@ -47,10 +47,10 @@ namespace TRA.Tool
             DBRef_GK5_to_EGBT22_Local,
             [Description("EGBT22_Local -> DBRef_GK5")]
             EGBT22_Local_to_DBRef_GK5,
-            //[Description("DBRef_GK5 -> ETRS89_UTM33")]
-            //DBRef_GK5_to_ETRS89_UTM33,
-            //[Description("ETRS89_UTM33 -> DBRef_GK5")]
-            //ETRS89_UTM33_to_DBRef_GK5
+            [Description("DBRef_GK5 -> ETRS89_UTM33")]
+            DBRef_GK5_to_ETRS89_UTM33,
+            [Description("ETRS89_UTM33 -> DBRef_GK5")]
+            ETRS89_UTM33_to_DBRef_GK5
         }
         private delegate (double Rechts, double Hoch, double H) SingleCoordinateTransform(double InRechts, double InHoch, double InH);
         private delegate bool ArrayCoordinateTransform(double[] InRechts, double[] InHoch, double[] InH, out double[] OutRechts, out double[] OutHoch, out double[] OutH);
@@ -154,22 +154,22 @@ namespace TRA.Tool
                     transformSetup.singleOut_Gamma_k = egbt22lib.Convert.DBRef_GK5_Gamma_k;
                     transformSetup.arrayOut_Gamma_K = egbt22lib.Convert.DBRef_GK5_Gamma_k;
                     break;
-                //case ETransforms.DBRef_GK5_to_ETRS89_UTM33:
-                //    transformSetup.singleCoordinateTransform = egbt22lib.Convert.DBRef_GK5_to_ETRS89_UTM33_Ell;
-                //    transformSetup.arrayCoordinateTransform = egbt22lib.Convert.DBRef_GK5_to_ETRS89_UTM33_Ell;
-                //    transformSetup.singleIn_Gamma_k = egbt22lib.Convert.DBRef_GK5_Gamma_k;
-                //    transformSetup.arrayIn_Gamma_K = egbt22lib.Convert.DBRef_GK5_Gamma_k;
-                //    transformSetup.singleOut_Gamma_k = egbt22lib.Convert.ETRS89_UTM33_Gamma_k;
-                //    transformSetup.arrayOut_Gamma_K = egbt22lib.Convert.ETRS89_UTM33_Gamma_k;
-                //    break;
-                //case ETransforms.ETRS89_UTM33_to_DBRef_GK5:
-                //    transformSetup.singleCoordinateTransform = egbt22lib.Convert.ETRS89_UTM33_to_DBRef_GK5_Ell;
-                //    transformSetup.arrayCoordinateTransform = egbt22lib.Convert.ETRS89_UTM33_to_DBRef_GK5_Ell;
-                //    transformSetup.singleIn_Gamma_k = egbt22lib.Convert.ETRS89_UTM33_Gamma_k;
-                //    transformSetup.arrayIn_Gamma_K = egbt22lib.Convert.ETRS89_UTM33_Gamma_k;
-                //    transformSetup.singleOut_Gamma_k = egbt22lib.Convert.DBRef_GK5_Gamma_k;
-                //    transformSetup.arrayOut_Gamma_K = egbt22lib.Convert.DBRef_GK5_Gamma_k;
-                //    break;
+                case ETransforms.DBRef_GK5_to_ETRS89_UTM33:
+                    transformSetup.singleCoordinateTransform = egbt22lib.Convert.DBRef_GK5_to_ETRS89_UTM33_Ell;
+                    transformSetup.arrayCoordinateTransform = egbt22lib.Convert.DBRef_GK5_to_ETRS89_UTM33_Ell;
+                    transformSetup.singleIn_Gamma_k = egbt22lib.Convert.DBRef_GK5_Gamma_k;
+                    transformSetup.arrayIn_Gamma_K = egbt22lib.Convert.DBRef_GK5_Gamma_k;
+                    transformSetup.singleOut_Gamma_k = egbt22lib.Convert.ETRS89_UTM33_Gamma_k;
+                    transformSetup.arrayOut_Gamma_K = egbt22lib.Convert.ETRS89_UTM33_Gamma_k;
+                    break;
+                case ETransforms.ETRS89_UTM33_to_DBRef_GK5:
+                    transformSetup.singleCoordinateTransform = egbt22lib.Convert.ETRS89_UTM33_to_DBRef_GK5_Ell;
+                    transformSetup.arrayCoordinateTransform = egbt22lib.Convert.ETRS89_UTM33_to_DBRef_GK5_Ell;
+                    transformSetup.singleIn_Gamma_k = egbt22lib.Convert.ETRS89_UTM33_Gamma_k;
+                    transformSetup.arrayIn_Gamma_K = egbt22lib.Convert.ETRS89_UTM33_Gamma_k;
+                    transformSetup.singleOut_Gamma_k = egbt22lib.Convert.DBRef_GK5_Gamma_k;
+                    transformSetup.arrayOut_Gamma_K = egbt22lib.Convert.DBRef_GK5_Gamma_k;
+                    break;
                 default:
                     transformSetup.singleCoordinateTransform = egbt22lib.Convert.DBRef_GK5_to_EGBT22_Local_Ell;
                     transformSetup.arrayCoordinateTransform = egbt22lib.Convert.DBRef_GK5_to_EGBT22_Local_Ell;
