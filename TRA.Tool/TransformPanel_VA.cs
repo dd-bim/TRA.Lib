14e2e866afc225a441ef773a9dd7dcd74f63528d
EMS90 <8hggitMetal>
2025-08-22 10:59:02 +0200

    Changes to new valib API

--- TRA.Tool/TransformPanel_VA.cs
+++ TRA.Tool/TransformPanel_VA.cs
@@ -33,63 +33,12 @@ namespace TRA.Tool
         {
             InitializeComponent();
             this.label_Panel.Text = "Transform VA";
-            //Load available Options
-            List<ComboBoxItem> options = new List<ComboBoxItem>();
-            foreach (valib.CRS value in Enum.GetValues(typeof(valib.CRS)))
-            {
-                options.Add(new ComboBoxItem(value.ToString()));
-            }
-            comboBox_TransformFrom.Items.AddRange(options.ToArray());
-            comboBox_TransformTo.Items.AddRange(options.ToArray());
-            //comboBox_TransformFrom.SelectedItem = valib.Convert.CRS.Bessel_GK2;
-            //Load Target CRS from CSV
-            string relativePath = @"dbref_va_syst.csv";
-            string fullPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, relativePath);
-            LoadOptionsFromCSV(fullPath);
+            comboBox_TransformFrom.Items.AddRange(valib.Convert.Defined_CRS);
+            comboBox_TransformTo.Items.AddRange(valib.Convert.Defined_CRS);
+            comboBox_TransformFrom.SelectedIndex = 1;
             comboBox_TransformFrom.SelectedIndexChanged += comboBox_Transform_SelectedIndexChanged;
             comboBox_TransformTo.SelectedIndexChanged += comboBox_Transform_SelectedIndexChanged;
             comboBox_Transform_SelectedIndexChanged(this, EventArgs.Empty);
-        }
-
-        class ComboBoxItem
-        {
-            public ComboBoxItem(string Name, string VA_ID = "", string fullString = "")
-            {
-                this.Name = Name;
-                this.VA_ID = VA_ID;
-                FullString = (fullString != "" ? fullString : Name);
-            }
-            public string VA_ID { get; set; }
-            public string Name { get; set; }
-            public string FullString { get; set; }
-            public override string ToString()
-            {
-                return Name + " (" + VA_ID + ")"; // Display "Name" in the ComboBox
-            }
-        }
-
-        private void LoadOptionsFromCSV(string filePath)
-        {
-            try
-            {
-                // Read all lines from the CSV file
-                var lines = File.ReadAllLines(filePath).Skip(1);
-
-                // Loop through each line and add it to the ComboBox
-                foreach (string line in lines)
-                {
-                    // Optionally, split by delimiter for multi-column CSV files
-                    string[] values = line.Split(',');
-                        // Add a specific column or the full line to the ComboBox
-                    ComboBoxItem item = new ComboBoxItem(values[1], values[0],line);
-                    comboBox_TransformTo.Items.Add(item); // Assuming the first column contains the options
-                    comboBox_TransformFrom.Items.Add(item);
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Error loading options: {ex.Message}");
-            }
         }
 
         private TransformSetup transformSetup;
@@ -100,13 +49,13 @@ namespace TRA.Tool
         private void comboBox_Transform_SelectedIndexChanged(object sender, EventArgs e)
         {
             if (comboBox_TransformFrom.SelectedItem == null || comboBox_TransformTo.SelectedItem == null) return;
-            ComboBoxItem CRSFrom = (ComboBoxItem)comboBox_TransformFrom.SelectedItem;
-            ComboBoxItem CRSTo = (ComboBoxItem)comboBox_TransformTo.SelectedItem;
+            string CRSFrom = (string)comboBox_TransformFrom.SelectedItem;
+            string CRSTo = (string)comboBox_TransformTo.SelectedItem;
             string info;
-            bool result = valib.Convert.GetConversion(CRSFrom.FullString, CRSTo.FullString, out transformSetup.ConvertFunc, out info) 
-                && valib.Convert.GetGammaKInsideCalculation(CRSFrom.FullString, out transformSetup.GammaK_From)
-                && valib.Convert.GetGammaKInsideCalculation(CRSTo.FullString, out transformSetup.GammaK_To);
-            transformSetup.Target_CRS = CRSTo.FullString;
+            bool result = valib.Convert.GetConversion(CRSFrom, CRSTo, out transformSetup.ConvertFunc, out info) 
+                && valib.Convert.GetGammaKInsideCalculation(CRSFrom, out transformSetup.GammaK_From)
+                && valib.Convert.GetGammaKInsideCalculation(CRSTo, out transformSetup.GammaK_To);
+            transformSetup.Target_CRS = CRSTo;
             toolTip.SetToolTip(comboBox_TransformFrom, info);
             toolTip.SetToolTip(comboBox_TransformTo, info);
             btn_Transform.Enabled = result;
